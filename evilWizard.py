9b0ce337e62080cd5cd4d91971fe76006cf5e12d
Savojros6 <savojros6@gmail.com>
2026-02-11 19:29:02 -0600

    Update evilWizard.py

--- evilWizard.py
+++ evilWizard.py
@@ -1,5 +1,7 @@
-import random
-secret_number = random.randint (1, 10)
+from random import randint
+secret_number = randint (1, 10)
+
+# I don't fully understand random, but randint allows me to generate a random integer in a range.
 
 print(
 """
@@ -12,8 +14,26 @@ print(
 +================================+
 """)
 
-guess = int(input("Guess a number: "))
+# 'try' and 'except' are an exception handling mechanism.
+# 'try' attempts to run subsequent code, but if it fails
+# instead of the whole program failing, it immediately
+# executes the corresponding 'except' conditions instead.
+# It has to be formatted kind of like an else/if statement.
 
+# 'ValueError' is a built-in exception class in Python.
+# It is raised when a function or operation receives an argument
+# of the correct type but with an inappropriate value.
+# e.g. attempting to turn a string into an integer when it can't be.
+
+# 'ValueError' being there isn't actually necessary its just clearer.
+
+while True:
+    try:
+        guess = int(input("Guess a number: "))
+        break
+    except ValueError:
+        print("Foolish muggle! That's not even a real number! Try again!!!")
+    
 while guess != secret_number:
     if guess > secret_number:
         print("Too high muggle, guess again!")
@@ -22,5 +42,4 @@ while guess != secret_number:
         print("Too low muggle, guess again!")
         guess = int(input("Guess a number: "))
         
-
 print('"Argh, drats! Curse you mortal, now I shall disintegrate into comsic ash! AAAAAAAAA"\nYou have killed the wizard...')
