06f533d699e277f63da31c08e5825340efdec0fe
Savojros6 <savojros6@gmail.com>
2026-03-05 19:12:36 -0600

    Update boiledPizza.py
    function

--- boiledPizza.py
+++ boiledPizza.py
@@ -12,35 +12,31 @@ ingred = ["tomato", "mushroom", "cheese", "pepperoni", "pineapple"]
 
 i = 0
 
+def prepare():
+    global i
+    time.sleep(1)
+    for seconds in range(1, 4):
+        print(f"{seconds} seconds.")
+        time.sleep(1)
+    print(f"Prepared {ingred[i]}.")
+    i += 1
+    time.sleep(1)
+    
+# Without the words 'global i', this code will break
+# The reason it breaks is it decides i is local to the function
+# due to the 'i += 1' line. Preventing it from reading and writing
+# to the globally declared 'i' variable already declared.
+
 while True:
     if i == 0:
-        print(f"First, we boil {ingred[i]} into the soup.")
-        time.sleep(1)
-        for seconds in range(1, 4):
-            print(f"{seconds} seconds.")
-            time.sleep(1)
-        print(f"Prepared {ingred[i]}.")
-        i += 1
-        time.sleep(1)
+        print(f"First, we boil {ingred[i]} into the soup.")        
+        prepare()
     elif i > 0 and i < len(ingred):
         print(f"Then we boil {ingred[i]} into the soup.")
-        time.sleep(1)
-        for seconds in range(1, 4):
-            print(f"{seconds} seconds.")
-            time.sleep(1)
-        print(f"Prepared {ingred[i]}.")
-        i += 1
-        time.sleep(1)
+        prepare()
     elif i == len(ingred) - 1:
         print(f"Finally, we boil {ingred[i]} into the soup.")
-        time.sleep(1)
-        for seconds in range(1, 4):
-            print(f"{seconds} seconds.")
-            time.sleep(1)
-        print(f"Prepared {ingred[i]}.")
-        i += 1
-        time.sleep(1)
+        prepare()
     else:
         print("Mama mia, don't you just love authentic italian cuisine?")
         break
-
