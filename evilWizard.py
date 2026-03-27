14fe4e7599af226285325e0f5d42f7c87426241d
Savojros6 <savojros6@gmail.com>
2026-03-27 12:38:44 -0500

    Update evilWizard.py
    handles more situations

--- evilWizard.py
+++ evilWizard.py
@@ -1,18 +1,35 @@
 from random import randint
 secret_number = randint (1, 10)
-counter = 0
+counter 		= 0
 
-agree		= ["yes", "okay", "ok", "sure", "alright", "agree", "accept", "do", "yeah"]
-disagree	= ["no", "nope", "nah", "never", "stop", "quit", "exit", "don't", "disagree"]
-explicit	= ["fuck you", "kys", "gay", "bitch", "kill yourself", "go fuck yourself"]
+# Lists for strings the script is intended to be able to respond to.
+
+agree		= [ "y", "yes", "okay", "ok", "sure", "alright", "agree", "accept", "do", "yeah", "ok fine", "okay fine"]
+disagree	= [ "n", "no", "nope", "nah", "never", "stop", "quit", "exit", "don't", "disagree"]
+explicit	= ["fuck you", "kys", "gay", "bitch", "bitche", "kill yourself", "go fuck yourself", "i hate you"]
+funny		= ["67"]
+
+# This list tracks your previous guesses, prevents duplicates from
+# incrementing the counter.
+
 prevGuess	= []
+
+# These variables will only come into play in the event I get
+# The ability to choose whether or not you want to replay the
+# game at the end working. They are currently unused.
+
+consent 		= 0
+victory 		= False
+secretJewels 	= 0
+
+# This is just the function that plays when the player actually wins or loses.
+# Its intended to clear prevGuess and reset the counter in the event the user
+# replays the number game.
 
 def gameOver():
     print(f'Previous guesses: {prevGuess}')
     del prevGuess[:]
     counter = 0
-    
-# It would be nice if it prompted you to try again.
 
 print(
 """
@@ -23,9 +40,46 @@ print(
 | picked for you.                |
 | The number is between 1 and 10.|
 | But you only get 3 guesses!    |
-| So, what is the secret number? |
+| If you win, you get some of my |
+| Secret Jewels, but if you lose,|
+| you DIE to my MAJYKK >:)       |
+| So, care to play my game?      |
 +================================+
 """)
+
+# The requirement that you consent to the wizards game
+# was implemented so that there would be a choice that preceded
+# the loop of the game. The idea is that if I define the loop of the entire game
+# into a function initiated as a result of a choice preceding the game
+# I might be able to get the replayability of the game working?
+# This is probably the right idea but I havent worked out the specifics yet.
+
+while True:
+    consent = input("Play the wizards game?: ")
+    if consent in agree:
+        print("""
++================================+
+| Great, now, what is your first |
+| guess mortal? >:)              |
++================================+
+        """)
+        break
+    
+    elif consent in disagree:
+        print("""
++================================+
+| WRONG, TRY AGAIN DUMBASS >:(   |
++================================+
+        """)
+        
+    else:
+        print("""
++================================+
+| Speak Normal EDIOT >:(         |
++================================+
+        """)
+
+# This is the section where it responds to strings in lists it has responses too.
 
 while True:
     try:
@@ -38,16 +92,42 @@ while True:
         # It's helpful when looking for specific strings.
         
         if guess.lower() in disagree:
-            print("Stupid BITCHE... Guess your number... Before I draw my bladde...")
+            print("""
++================================+
+| Stupid BITCHE... Guess your    |
+| number... Before I draw my     |
+| bladde..."                     |
++================================+
+        """)
             continue
         
         elif guess.lower() in agree:
-            print("Ver naisu. Now, let's begin! :)")
+            print("""
++================================+
+| Ver naisu. Now, let's begin! :)|
++================================+
+        """)
             continue
         
         elif guess.lower() in explicit:
-            print("wtfug! mean!!!")
+            print("""
++================================+
+| wtfug! mean!!! D:              |
++================================+
+        """)
             continue
+        
+        elif guess in funny:
+            print("""
++================================+
+| You've posted CRINGE mortal... |
+| You are UNWORTHY of my game... |
+| DIE...                         |
++================================+
+        """)
+            print("You have been cut down by the wizards kata anna...")
+            gameOver()
+            break
                 
         # The reason this has to end w/ 'elif' and not 'else' is I think 'else' can't have conditions?
         
@@ -56,43 +136,117 @@ while True:
 
     try:
         guess = int(guess)
-        
+
+# I thought it'd be nice to acknowledge if you ruined the wizards game by guessing it immediately.
+
         if guess == secret_number and counter == 0:
             prevGuess.append(guess)
-            print('"Gadzooks! How could you have know the Legendary Secret Number already?! You MUST have cheated!"\n"The wizard malds as he slowly transmogrifies into a corn cob"')
+            print("""
++================================+
+| Gadzooks! How could you have   |
+| known the Legendary Secret     |
+| Number already?! You MUST have |
+| cheated!                       |
++================================+
+        """)             
+            print("The wizard malds impotently as he slowly transmogrifies into a corn cob.")
             gameOver()
+            victory = True
             break
         
         elif guess == secret_number and counter > 0:
             prevGuess.append(guess)
-            print('"Argh, drats! Curse you mortal, now I shall disintegrate into comsic ash! AAAAAAAAA"\nYou have killed the wizard...')
+            print("""
++================================+
+| Argh, drats! Curse you mortal, |
+| now I shall disintegrate into  |
+| comsic ash! AAAAAAAAAAAAAAAAAAA|
++================================+
+        """)                        
+            print("You have killed the wizard...")
             gameOver()
+            victory = True
             break
         
         elif counter == 2 and guess != secret_number:
             prevGuess.append(guess)
-            print('"Too late FOOL, you LOSE! Prepare to face my OMEGA BEANS!!!"\nYou were disintegrated by the wizards majykks...')
+            print("""
++================================+
+| Too late FOOL, you LOSE!       |
+| Prepare to face my OMEGA       |
+| BEANS!!!                       |
++================================+
+        """)
+            print("You were disintegrated by the wizards majykks...")
             gameOver()
             break
-        
+
+# This is mostly so that repeat guesses can't increment the counter.
+
         elif guess in prevGuess:
-            print("You already SAID that one!! >:(")
+            print("""
++================================+
+| You already SAID that one! >:( |
++================================+
+        """)
         
+# This was something already covered by other parts of the cascade
+# but I thought it was something that deserved its own response.
+            
+        elif guess <= 0 or guess >= 11:
+            print("""
++================================+
+| FOOLE, it OBVIOUSLY can't be   |
+| that one! Are you even listen- |
+| -ing to me?!                   |
++================================+
+        """)
+            prevGuess.append(guess)
+
+# The original idea that prompted me to keep developing this script was that
+# the original example code for this would just loop until you said '777'
+# which I don't think qualifies as a game. So I made it 3 tries, within
+# the range of 1 and 10, and it tells you higher or lower. Which makes it
+# an incredibly lame game, but it makes it a game.
+
+# It might be funny if the wizard calls you stupid if you guess higher
+# after he already told you to guess lower or vice versa.
+
+# The wizard should totally accuse you of being a metafag if you
+# guess 5 or 6 first.
+
         elif guess > secret_number:
-            print("Too high mortal, guess again!")
+            print("""
++================================+
+| Too high mortal, guess again!  |
++================================+
+        """)            
             prevGuess.append(guess)
             counter += 1
             
         elif guess < secret_number:
-            print("Too low mortal, guess again!")
+            print("""
++================================+
+| Too low mortal, guess again!   |
++================================+
+        """)       
             prevGuess.append(guess)
             counter += 1
-        
+
+# I genuinely can't think of a way to trigger this condition, but I guess thats the point.
+
         else:
             print("???")
-            
-    except ValueError:
-        print("Foolish mortal! That's not even a real number! Try again!!!")        
+
+# If the input isn't a number or a string the loop can respond to this is what you get.
+
+    except ValueError:        
+            print("""
++================================+
+| Foolish mortal! That's not even|
+| a real number! Try again!!!    |
++================================+
+        """)   
     
 # ====================================================================#
 # 'try' and 'except' are an exception handling mechanism.             |
@@ -107,4 +261,4 @@ while True:
 # e.g. attempting to turn a string into an integer when it can't be.  |
 #                                                                     |
 # 'ValueError' being there isn't actually necessary its just clearer. |
-# ====================================================================# 
+# ====================================================================#   
